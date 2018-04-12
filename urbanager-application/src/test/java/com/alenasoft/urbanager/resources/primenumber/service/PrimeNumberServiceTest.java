5f644aa8accadf02b612b43610369eb5e76608a8
Gustavo <gujtavitocr@gmail.com>
2018-04-12 02:08:52 -0400

    Paquetes y clase de prueba creada.

---
+++ urbanager-application/src/test/java/com/alenasoft/urbanager/resources/primenumber/service/PrimeNumberServiceTest.java
@@ -0,0 +1,17 @@
+package com.alenasoft.urbanager.resources.primenumber.service;
+
+import com.alenasoft.urbanager.api.Example;
+import com.alenasoft.urbanager.resources.example.dao.ExampleDao;
+import com.alenasoft.urbanager.resources.example.service.ExampleService;
+import com.alenasoft.urbanager.resources.example.service.ExampleServiceImpl;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrimeNumberServiceTest {
+}
